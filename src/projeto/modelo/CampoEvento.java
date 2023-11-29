16597f9a7636056878ed0efba5d74aba4ba1a4c6
João Marcos <00776630202@DT-120020361>
2023-11-29 09:42:53 -0400

    Subir projeto

---
+++ src/projeto/modelo/CampoEvento.java
@@ -0,0 +1,5 @@
+package projeto.modelo;
+
+public enum CampoEvento {
+    ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
+}
---
+++ src/projeto/modelo/CampoObservador.java
@@ -0,0 +1,5 @@
+package projeto.modelo;
+
+public interface CampoObservador {
+    public void eventoOcorreu(Campo campo, CampoEvento evento);
+}
